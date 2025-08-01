1e1c893f01db8f3c6367cd2b4c2e55182146a621
derwin9227 <derwin9227@gmail.com>
2025-07-31 20:14:53 -0500

    adding list of friends

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,45 @@
-import "./App.css";
 import initialFriends from "./App";
-function App() {
+
+export default function App() {
   return (
-    <>
-      <div>eat'n split</div>
-    </>
+    <div className="app">
+      <div className="sidebar">
+        <FriendList />
+      </div>
+    </div>
   );
 }
 
-export default App;
+function FriendList() {
+  const friends = initialFriends;
+
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend key={friend.id} friend={friend} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.nameq} />
+      {friend.name}
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} ${Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you ${Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+      <button className="button">Select</button>
+    </li>
+  );
+}
